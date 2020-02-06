a291dec2a502ae9538b63a803c3d05b61af3c70e
yngtodd <young.todd.mk@gmail.com>
2020-02-06 12:07:51 -0500

    Fix missing imports
    Was forgetting to add RPC imports.

--- examples/model_parallel.py
+++ examples/model_parallel.py
@@ -1,10 +1,16 @@
+import os
+
 import torch
 import torch.nn.functional as F
+import torch.multiprocessing as mp
+import torch.distributed.rpc as rpc
+from torch.distributed.rpc import RRef
+from torch.distributed.rpc import rpc_sync
+from torch.distributed.optim import DistributedOptimizer
 
 from torch_geometric.datasets import Reddit
 from torch_geometric.data import NeighborSampler
 from torch_geometric.nn import SAGEConv, GATConv
-from torch.distributed.rpc import rpc_sync
 
 
 def _call_method(method, rref, *args, **kwargs):
@@ -65,13 +71,13 @@ class GRPCNet(torch.nn.Module):
     """ Model parallel GCN  """
 
     def __init__(self, ps, in_channels, out_channels):
-        super(GATNet, self).__init__()
+        super(GRPCNet, self).__init__()
         self.gat_in = GATin(in_channels)
         self.gat_out_rref = rpc.remote(ps, GATout, args=(out_channels))
 
     def forward(self, x, data_flow):
         x, block = self.gat_in(x, data_flow)
-        x = _remote_method(GATout.forward self.gat_out_rref, x, block)
+        x = _remote_method(GATout.forward, self.gat_out_rref, x, block)
         return x
 
     def parameter_rrefs(self):
@@ -79,8 +85,10 @@ class GRPCNet(torch.nn.Module):
         remote_params = []
         # create RRefs for local parameters
         remote_params.extend(_parameter_rrefs(self.gat_in))
+        print('I have the gat_in params')
         # get RRefs of the GATout module 
         remote_params.extend(_remote_method(_parameter_rrefs, self.gat_out_rref))
+        print('I have the gat_out params')
         return remote_params
 
 
@@ -106,7 +114,7 @@ def test(mask, model, loader, device):
     return correct / mask.sum().item()
 
 
-def main():
+def run_trainer():
 
     dataset = Reddit('~/data')
     data = dataset[0]
@@ -116,14 +124,42 @@ def main():
     )
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = GRPCNet(dataset.num_features, dataset.num_classes).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+    model = GRPCNet("ps", dataset.num_features, dataset.num_classes).to(device)
+    #optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+    # setup distributed optimizer
+    optimizer = DistributedOptimizer(
+        torch.optim.SGD,
+        model.parameter_rrefs(),
+        lr=0.05,
+    )
 
     for epoch in range(1, 31):
-    loss = train(model, loader, optimizer, device)
-    test_acc = test(data.test_mask, model, loader, device)
-    print('Epoch: {:02d}, Loss: {:.4f}, Test: {:.4f}'.format(
-        epoch, loss, test_acc))
+        loss = train(model, loader, optimizer, device)
+        test_acc = test(data.test_mask, model, loader, device)
+        print('Epoch: {:02d}, Loss: {:.4f}, Test: {:.4f}'.format(
+              epoch, loss, test_acc))
 
 
+def run_worker(rank, world_size):
+    r"""
+    A wrapper function that initializes RPC, calls the function, and shuts down
+    RPC.
+    """
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '29500'
+    if rank == 1:
+        rpc.init_rpc("trainer", rank=rank, world_size=world_size)
+        run_trainer()
+    else:
+        rpc.init_rpc("ps", rank=rank, world_size=world_size)
+        # parameter server do nothing
+        pass
 
+    # block until all rpcs finish
+    rpc.shutdown()
+
+
+if __name__=="__main__":
+    world_size = 2
+    mp.spawn(run_worker, args=(world_size, ), nprocs=world_size, join=True)
